8d9487f298aefb6d6a51972431bb96a5d99fd4b2
Jimena-belen <jimena.morano@branded.com.ar>
2023-05-16 23:49:42 -0300

    refactor de converter

--- src/main/java/ar/com/branded/bfftransaction/util/converters/ConverterProductToEntity.java
+++ src/main/java/ar/com/branded/bfftransaction/util/converters/ConverterProductToEntity.java
@@ -6,62 +6,65 @@ import ar.com.branded.bfftransaction.repository.model.jdbc.ProductsAndDetailsLis
 import ar.com.branded.bfftransaction.repository.model.rest.Image;
 import ar.com.branded.bfftransaction.repository.model.rest.Product;
 import ar.com.branded.bfftransaction.repository.model.rest.Variant;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConverterProductToEntity {
 
-    public static ProductsAndDetailsList execute(List<Product> products, String userId){
+    public static ProductsAndDetailsList execute(List<Product> products, String userId) {
         ProductsAndDetailsList productsAndDetailsList = new ProductsAndDetailsList();
-        List<ProductTable> productTableList = new ArrayList<>();
-        List<ProductDetailsTable> productDetailsTableList = new ArrayList<>();
+        List<ProductTable> productTableList = products.stream()
+                .map(product -> convertProductToProductTable(product, userId))
+                .collect(Collectors.toList());
 
-        products.forEach(product -> {
-                    ProductTable productTable = new ProductTable();
-                    String DEFAULT_IMAGE = product.getImages()[0].getSrc();
-                    String DEFAULT_PRICE = product.getVariants()[0].getPrice();
-                    Integer GENERAL_STOCK = Arrays
-                            .stream(product.getVariants())
-                            .toList()
-                            .stream()
-                            .map(Variant::getStock)
-                            .reduce(Integer::sum).orElse(0);
+        List<ProductDetailsTable> productDetailsTableList = products.stream()
+                .flatMap(product -> Arrays.stream(product.getVariants())
+                        .map(variant -> convertVariantToProductDetailsTable(product, variant)))
+                .collect(Collectors.toList());
 
-                    productTable.setId(product.getId());
-                    productTable.setName(product.getName().getEs());
-                    productTable.setUser_id(userId);
-                    productTable.setDescription(product.getDescription().getEs());
-                    productTable.setPrice(DEFAULT_PRICE);
-                    productTable.setImage_url(DEFAULT_IMAGE);
-                    productTable.setStock(GENERAL_STOCK.toString());
-                    productTableList.add(productTable);
-
-                    Arrays.stream(product.getVariants()).toList().forEach(variant -> {
-                        ProductDetailsTable productDetailsTable = new ProductDetailsTable();
-
-                        List<Image> imageVariant = Arrays
-                                .stream(product.getImages())
-                                .filter( image -> image.getId().equals(variant.getImage_id()) )
-                                .toList();
-
-                        productDetailsTable.setId(variant.getId());
-                        productDetailsTable.setProductId(product.getId().toString());
-                        productDetailsTable.setPrice(variant.getPrice());
-                        productDetailsTable.setStock(variant.getStock());
-                        productDetailsTable.setWeight(variant.getWeight());
-                        productDetailsTable.setDepth(variant.getDepth());
-                        productDetailsTable.setWidth(variant.getWidth());
-                        productDetailsTable.setHeight(variant.getHeight());
-                        productDetailsTable.setPromotional_price(variant.getPromotional_price());
-                        productDetailsTable.setImage_url(imageVariant.get(0).getSrc());
-
-                        productDetailsTableList.add(productDetailsTable);
-                    });
-                });
         productsAndDetailsList.setProducts(productTableList);
         productsAndDetailsList.setProductDetails(productDetailsTableList);
 
         return productsAndDetailsList;
+    }
+
+    private static ProductTable convertProductToProductTable(Product product, String userId) {
+        String defaultImage = product.getImages()[0].getSrc();
+        String defaultPrice = product.getVariants()[0].getPrice();
+        Integer generalStock = Arrays.stream(product.getVariants())
+                .mapToInt(Variant::getStock)
+                .sum();
+
+        ProductTable productTable = new ProductTable();
+        productTable.setId(product.getId());
+        productTable.setName(product.getName().getEs());
+        productTable.setUser_id(userId);
+        productTable.setDescription(product.getDescription().getEs());
+        productTable.setPrice(defaultPrice);
+        productTable.setImage_url(defaultImage);
+        productTable.setStock(generalStock.toString());
+
+        return productTable;
+    }
+
+    private static ProductDetailsTable convertVariantToProductDetailsTable(Product product, Variant variant) {
+        List<Image> imageVariant = Arrays.stream(product.getImages())
+                .filter(image -> image.getId().equals(variant.getImage_id()))
+                .collect(Collectors.toList());
+
+        ProductDetailsTable productDetailsTable = new ProductDetailsTable();
+        productDetailsTable.setId(variant.getId());
+        productDetailsTable.setProductId(product.getId().toString());
+        productDetailsTable.setPrice(variant.getPrice());
+        productDetailsTable.setStock(variant.getStock());
+        productDetailsTable.setWeight(variant.getWeight());
+        productDetailsTable.setDepth(variant.getDepth());
+        productDetailsTable.setWidth(variant.getWidth());
+        productDetailsTable.setHeight(variant.getHeight());
+        productDetailsTable.setPromotional_price(variant.getPromotional_price());
+        productDetailsTable.setImage_url(imageVariant.get(0).getSrc());
+
+        return productDetailsTable;
     }
 }
